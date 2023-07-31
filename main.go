fc5bd175826bfe859873473ef3a5a7f3398457f5
Ahbar <ahbar1234@gmail.com>
2023-07-31 15:39:19 +0530

    everything seems to be working as of now

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"math/rand"
 	"errors"
+	"sort"
 )
 
 type Player struct {
@@ -19,6 +20,7 @@ type Player struct {
 }
 
 var players = make([]*Player, 0)
+var newId = 0
 
 func newPlayer(name string, country string, score int) (*Player, error) {
 	if len(name) == 0 || len(name) > 15 {
@@ -27,13 +29,17 @@ func newPlayer(name string, country string, score int) (*Player, error) {
 		return nil, errors.New("country code invalid")
 	}
 	
-	players = append(players, &Player{len(players), name, country, score})
+	players = append(players, &Player{newId, name, country, score})
+	newId += 1
 	return players[len(players) - 1], nil 
 }
 
 func getPlayers(w http.ResponseWriter, r *http.Request) {
 	urlString := r.URL.String()
 	fmt.Println(urlString)
+	sort.Slice(players, func(i, j int) bool {
+		return players[i].Score > players[j].Score
+	})	
 	switch r.Method {
 	case "GET":
 		switch true {
@@ -84,18 +90,79 @@ func getPlayers(w http.ResponseWriter, r *http.Request) {
 		default:
 			w.WriteHeader(http.StatusNotFound)
 		}
-	/*	
+	
 	case "DELETE":
 		i := strings.Index(urlString, ":")
-			
-		id, err := strconv.Atoi(urlString[i + 1:])
+		deleted := false	
+		id, _ := strconv.Atoi(urlString[i + 1:])
 		for i, p := range players {
 			if p.Id == id {
-				// set player to nil		
+				// shrink slice
+				if i == len(players) - 1 {
+					players = players[:len(players) - 1]
+				} else {	
+					players = append(players[:i], players[i + 1:]...)
+				}
+				deleted = true
 			}
-		
 		}
-	*/
+		
+		if !deleted {
+			w.WriteHeader(http.StatusConflict)
+			w.Write([]byte(fmt.Sprintf("Player with id: : %v NOT FOUND", id))) 
+			return	
+		} else {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(fmt.Sprintf("Successfully deleted player with id: %v", id))) 
+		}
+	
+	case "POST":
+		r.ParseForm()
+		score, err := strconv.Atoi(r.Form.Get("score"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+		}
+
+		newPlayer(r.Form.Get("name"), r.Form.Get("country"), score)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Successfully added a new player"))
+	
+	case "PUT":
+		r.ParseForm()
+		i := strings.Index(urlString, ":")
+		if i == -1 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Send a request with ':id'"))
+			return
+		}
+		modified := false	
+		id, _ := strconv.Atoi(urlString[i + 1:])
+		for i, p := range players {
+			if p.Id == id {
+				
+				if r.Form.Has("name") {
+					players[i].Name = r.Form.Get("name")
+				} 
+				if r.Form.Has("score") {
+					score, _ := strconv.Atoi(r.Form.Get("score")) 
+					players[i].Score = score
+				} 
+				if r.Form.Has("name") || r.Form.Has("score") {
+					modified = true
+				}
+				break
+			}
+		}
+		
+		if !modified {
+			w.WriteHeader(http.StatusConflict)
+			w.Write([]byte(fmt.Sprintf("Player with id: : %v NOT FOUND", id))) 
+			return	
+		} else {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(fmt.Sprintf("Successfully modified player with id: %v", id))) 
+		}
 	}	
 }
 
@@ -108,14 +175,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	// register handlers
-	/*
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(r.URL.String()))
-	})
-	*/
-
-	mux.Handle("/", http.HandlerFunc(getPlayers))
+	mux.Handle("/players/", http.HandlerFunc(getPlayers))
 	
 	log.Fatal(http.ListenAndServe(":" + "8080", mux))
 }
